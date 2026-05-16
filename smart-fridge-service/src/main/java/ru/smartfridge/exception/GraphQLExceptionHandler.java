0a097515c1a7fb169bf20a65b978059c18c4d6cc
Danya K <danilkaryakin1202@mail.ru>
2026-05-16 19:33:07 +0300

    feat: add graphql exception handler

---
+++ smart-fridge-service/src/main/java/ru/smartfridge/exception/GraphQLExceptionHandler.java
@@ -0,0 +1,41 @@
+package ru.smartfridge.exception;
+
+import com.netflix.graphql.types.errors.TypedGraphQLError;
+import graphql.execution.DataFetcherExceptionHandler;
+import graphql.execution.DataFetcherExceptionHandlerParameters;
+import graphql.execution.DataFetcherExceptionHandlerResult;
+import org.springframework.stereotype.Component;
+import ru.smartfridge.contract.exception.ResourceNotFoundException;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class GraphQLExceptionHandler implements DataFetcherExceptionHandler {
+
+    @Override
+    public CompletableFuture<DataFetcherExceptionHandlerResult> handleException(
+            DataFetcherExceptionHandlerParameters handlerParameters) {
+
+        Throwable ex = handlerParameters.getException();
+
+        if (ex instanceof ResourceNotFoundException) {
+            var error = TypedGraphQLError.newNotFoundBuilder()
+                    .message(ex.getMessage())
+                    .path(handlerParameters.getPath())
+                    .build();
+
+            return CompletableFuture.completedFuture(
+                    DataFetcherExceptionHandlerResult.newResult().error(error).build()
+            );
+        }
+
+        var error = TypedGraphQLError.newInternalErrorBuilder()
+                .message("Внутренняя ошибка сервера")
+                .path(handlerParameters.getPath())
+                .build();
+
+        return CompletableFuture.completedFuture(
+                DataFetcherExceptionHandlerResult.newResult().error(error).build()
+        );
+    }
+}
